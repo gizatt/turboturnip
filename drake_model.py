107c2d3229fafcf571e0759393233ca306176a23
Greg Izatt <gizatt@mit.edu>
2020-06-01 21:52:39 -0400

    Add GMM and KDE versions

--- drake_model.py
+++ drake_model.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import matplotlib.cm as cm
 import numpy as np
-from scipy.stats import multivariate_normal
+from scipy import interpolate
+from scipy.stats import multivariate_normal, gaussian_kde
+from scipy.special import logsumexp
+from sklearn import mixture
 
 import time
 
@@ -20,27 +23,64 @@ class Rollout:
         self.prices = prices
         self.means = None
         self.vars = None
+        self.model_type = None
 
     def __repr__(self):
         s = "%s Rollout (weighted %0.3f): " % (self.pattern_name, self.rollout_probability)
         s += "\n\t" + str(self.prices) + "\n"
         return s
 
-    def fit_normal(self, num_samples):
+    def fit_model(self, num_samples, model_type="gmm", model_params={}):
         g = pydrake.common.RandomGenerator()
         v = [sym.Evaluate(self.prices, generator=g) for i in range(num_samples)]
         self.v = np.stack(v).reshape(num_samples, 12)
-        self.means = np.mean(self.v, axis=0)
-        self.vars = np.var(self.v, axis=0)
 
-    def evaluate_pdf(self, x):
-        assert(len(x) == 12)
+        self.model_type = model_type
+        if model_type == "gmm":
+            assert("n_components" in model_params.keys())
+            n_components = model_params["n_components"]
+            self.g = mixture.GaussianMixture(n_components=n_components, covariance_type='diag')
+            self.g.fit(self.v)
+        elif model_type == "kde":
+            assert("bw_method" in model_params)
+            self.kdes = [gaussian_kde(self.v[:, k], bw_method=model_params["bw_method"]) for k in range(12)]
+        else:
+            raise NotImplementedError("Model type %s" % model_type)
+    
+    def evaluate_componentwise_pdf(self, vals):
+        assert(self.model_type is not None)
+        assert(self.model_type == "gmm")
+        scores = []
+        for k in range(12):
+            scores.append(
+                sum([multivariate_normal.pdf(vals, self.g.means_[comp_k, k],
+                     self.g.covariances_[comp_k, k])*self.g.weights_[comp_k]
+                        for comp_k in range(len(self.g.weights_))]))
+        return np.stack(scores)
+
+    def evaluate_logpdf(self, x):
+        assert(self.model_type is not None)
+        assert(len(x) == 13)
         # Values of X can be float or None
-        total_density = 1.
-        for k, val in enumerate(x):
-            if val is not None:
-                total_density *= multivariate_normal.pdf(val, mean=self.means[k], cov=self.vars[k])
-        return total_density
+        if self.model_type == "gmm":
+            total_log_density = 0.
+            logscores_by_comp = self.g._estimate_log_prob(np.array(x[1:]).reshape(1, -1)).flatten()
+            log_weights = self.g._estimate_log_weights().flatten()
+            # Correct those scores for unobserved points, which we can do because
+            # of the diagonal covariances
+            for k, val in enumerate(x[1:]):
+                if val == 0:
+                    for comp_k in range(len(logscores_by_comp)):
+                        logscores_by_comp[comp_k] -= multivariate_normal.logpdf(
+                            val, self.g.means_[comp_k, k],
+                            self.g.covariances_[comp_k, k])
+            return logsumexp(logscores_by_comp + log_weights)
+        elif self.model_type == "kde":
+            total_log_density = 0.
+            for k, val in enumerate(x[1:]):
+                if val != 0:
+                    total_log_density += self.kdes[k].logpdf(val)
+            return total_log_density
 
 uniform_variable_store = {}
 def random_uniform(name, low, high):
@@ -62,6 +102,14 @@ pattern_transition_matrix = np.array(
      [0.5, 0.05, 0.2, 0.25],
      [0.25, 0.45, 0.05, 0.25],
      [0.45, 0.25, 0.15, 0.15]]).T
+
+colors_by_type = {"Large Spike": np.array([0., 0., 1]), "Decreasing": np.array([1., 0., 0.]),
+                 "Small Spike": np.array([0., 1., 0.]), "Random": np.array([1., 0., 1.]),
+                 "Average Prediction": np.array([0.1, 0.1, 0.1])}
+colormaps_by_type = {"Large Spike": cm.get_cmap('winter'),
+                     "Decreasing": cm.get_cmap('spring'),
+                     "Small Spike": cm.get_cmap('summer'),
+                     "Random": cm.get_cmap('cool')}
 
 def generate_previous_pattern_steady_state():
     # The prior over patterns is the steady state of
@@ -145,7 +193,8 @@ def generate_all_turnip_patterns(previous_pattern_prior, base_price=None):
     all_rollouts = []
 
     # Random integer base price on [90, 110]
-    if base_price is None:
+    print("Input base price: ", base_price)
+    if base_price == 0:
         base_price = random_uniform("base_price", 90, 110)
     else:
         base_price = base_price
@@ -160,87 +209,176 @@ def generate_all_turnip_patterns(previous_pattern_prior, base_price=None):
 
     return all_rollouts
 
-if __name__ == "__main__":
-    #observed = [None, None,  # M
-    #        None, None,  # T
-    #        None, None,  # W
-    #        None, None,  # R
-    #        None, None,  # F
-    #        None, None]  # S
+def plot_gmm_results(valid_rollouts, observed):
+    # Combine the valid rollouts into a final model over prices
+    # by combining the modes
+    all_means = []
+    all_covars = []
+    all_weights = []
+    for rollout in valid_rollouts:
+        all_means.append(rollout.g.means_)
+        all_covars.append(rollout.g.covariances_)
+        all_weights.append(rollout.g.weights_)
+    all_means = np.vstack(all_means)
+    all_covars = np.vstack(all_covars)
+    all_weights = np.concatenate(all_weights)
+    all_weights /= np.sum(all_weights)
+    for k, val in enumerate(observed[1:]):
+        if val != 0:
+            all_means[:, k] = val
+            all_covars[:, k] = 0.
 
-    observed = [66, 61,  # M
+    # Finally, draw it
+    plt.figure()
+    plt.grid(True)
+
+    grid_cells = np.arange(0., 660., 1.)
+
+    total_prob_image = np.zeros((12, grid_cells.shape[0]))
+    for k, rollout in enumerate(valid_rollouts):
+        total_prob_image += rollout.evaluate_componentwise_pdf(grid_cells)
+        
+        for comp_k in range(len(rollout.g.weights_)):
+            mean = rollout.g.means_[comp_k, :]
+            covar = rollout.g.covariances_[comp_k, :]
+            plt.plot(range(12), mean,
+                     color=colors_by_type[rollout.pattern_name],
+                     alpha=0.5, linestyle="--")
+            plt.fill_between(
+                range(12), mean-1.96*np.sqrt(covar), mean+1.96*np.sqrt(covar),
+                alpha=0.5, edgecolor=colors_by_type[rollout.pattern_name],
+                facecolor=colormaps_by_type[rollout.pattern_name](float(k)/len(valid_rollouts)))
+        print(rollout.prices)
+        #plt.plot(range(12), rollout.v.T,
+        #         color=colors_by_type[rollout.pattern_name],
+        #         alpha=0.01, linestyle="--")
+    #interpolate.interp2d(total_prob_image
+    #plt.imshow(total_prob_image.T, origin='lower', extent=[0, 11, 0, 660], aspect='auto', cmap="summer", interpolation="bilinear")
+
+    for comp_k in range(len(all_weights)):
+        mean = all_means[comp_k, :]
+        covar = all_covars[comp_k, :]
+        weight = all_weights[comp_k]
+        #plt.plot(range(12), mean, 'k', color=colors_by_type["Average Prediction"])
+        #plt.fill_between(range(12), mean-1.96*np.sqrt(covar), mean+1.96*np.sqrt(covar),
+        #    alpha=0.2, edgecolor=colors_by_type["Average Prediction"], facecolor=[0.8, 0.1, 0.8], hatch="+")
+    # mean of means
+    plt.plot(range(12), np.sum(all_weights.T*all_means.T, axis=1), color=colors_by_type["Average Prediction"], alpha=0.5)
+
+    ks = []
+    vals = []
+    for k, val in enumerate(observed):
+        if val != 0:
+            ks.append(k)
+            vals.append(val)
+    plt.scatter(ks, vals, color="k")
+
+    label_lines = [Line2D([0], [0], color=colors_by_type[pattern_name], lw=4) for pattern_name in list(colors_by_type.keys())]
+    plt.legend(label_lines, list(colors_by_type.keys()))
+
+def plot_kde_results(valid_rollouts, observed):
+    # Finally, draw it
+    plt.figure()
+    plt.grid(True)
+
+    grid_cells = np.arange(0., 660., 5.)
+
+    all_prob_images = []
+    regions = []
+    for k, rollout in enumerate(valid_rollouts):
+        this_prob_image = np.vstack([kde.logpdf(grid_cells) for kde in rollout.kdes])
+        region_bin = (this_prob_image > -1e3)
+        region_low = grid_cells[np.argmax(region_bin, axis=1)]
+        region_high = grid_cells[-1 - np.argmax(np.flip(region_bin, axis=1), axis=1)]
+        regions.append((region_low, region_high))
+        all_prob_images.append(this_prob_image + rollout.rollout_probability)
+
+    total_prob_image = sum(all_prob_images) - logsumexp(all_prob_images)
+    maxval = np.max(total_prob_image)
+    minval = maxval - 1E-3
+    plt.imshow(np.log(-total_prob_image.T),
+               origin='lower', extent=[0, 11, 0, 660], aspect='auto', cmap="YlOrRd", interpolation="bilinear",
+               alpha=1.0)
+
+    maxes = grid_cells[np.argmax(total_prob_image, axis=1)]
+    plt.plot(range(12), maxes, color=colors_by_type["Average Prediction"], alpha=0.5)
+    print("Maxes: ", maxes)
+
+    # Push to after initial imshow so these are on top
+    for k, rollout in enumerate(valid_rollouts):
+        plt.fill_between(
+            range(12), regions[k][0], regions[k][1],
+            alpha=0.25, edgecolor=colors_by_type[rollout.pattern_name],
+            facecolor=colormaps_by_type[rollout.pattern_name](float(k)/len(valid_rollouts)))
+        
+    ks = []
+    vals = []
+    for k, val in enumerate(observed):
+        if val != 0:
+            ks.append(k)
+            vals.append(val)
+    plt.scatter(ks, vals, color="k")
+
+    label_lines = [Line2D([0], [0], color=colors_by_type[pattern_name], lw=4) for pattern_name in list(colors_by_type.keys())]
+    plt.legend(label_lines, list(colors_by_type.keys()))
+
+if __name__ == "__main__":
+    model_type = "gmm"
+    model_params = {"n_components": 1,
+                    "bw_method": 0.05}
+    observed = [109, # base price
+            66, 61,  # M
             58, 53,  # T
             48, 114,  # W
             122, 137,  # R
-            None, None,  # F
-            None, None]  # S
-    base_price = 109
+            138, 136,  # F
+            0, 0]  # S
 
+    #observed = [109, # base price
+    #        98, 94,  # M
+    #        0, 0,  # T
+    #        0, 0,  # W
+    #        0, 0,  # R
+    #        0, 0,  # F
+    #        0, 0]  # S
+
+    #observed_data = np.loadtxt("example_data.csv", dtype=int, delimiter=",", skiprows=1, usecols=range(1, 1+13))
+    #observed = observed_data[11, :]
+
+    print("Observed: ", observed)
     previous_pattern_prior = generate_previous_pattern_steady_state()
-    all_rollouts = generate_all_turnip_patterns(previous_pattern_prior, base_price=base_price)
+    all_rollouts = generate_all_turnip_patterns(previous_pattern_prior, observed[0])
     
     g = pydrake.common.RandomGenerator()
     
     start = time.time()
     num_samples = 1000
-    colors_by_type = {"Large Spike": np.array([0., 0., 1]), "Decreasing": np.array([1., 0., 0.]),
-                     "Small Spike": np.array([0., 1., 0.]), "Random": np.array([1., 0., 1.]),
-                     "Average Prediction": np.array([0.1, 0.1, 0.1])}
-    colormaps_by_type = {"Large Spike": cm.get_cmap('winter'),
-                         "Decreasing": cm.get_cmap('spring'),
-                         "Small Spike": cm.get_cmap('summer'),
-                         "Random": cm.get_cmap('cool')}
 
     # Fit a multivariate normal distribution to each rollout
     for rollout in all_rollouts:
-        rollout.fit_normal(num_samples)
-
+        rollout.fit_model(
+            num_samples,
+            model_type=model_type,
+            model_params=model_params)
+        
     # Adjust the probability of each rollout based on the normal dist
     for rollout in all_rollouts:
-        density = rollout.evaluate_pdf(observed)
-        rollout.rollout_probability *= density
-        print("New prob: ", rollout.rollout_probability)
+        log_density = rollout.evaluate_logpdf(observed)
+        rollout.rollout_probability = np.log(rollout.rollout_probability) + log_density
+        print("New log prob: ", rollout.rollout_probability)
     
     # Normalize probabilities across remaining rollouts
-    total_prob = sum([rollout.rollout_probability for rollout in all_rollouts])
+    total_log_prob = logsumexp([rollout.rollout_probability for rollout in all_rollouts])
     valid_rollouts = []
     for rollout in all_rollouts:
-        rollout.rollout_probability /= total_prob
-        if rollout.rollout_probability > 1E-4:
+        rollout.rollout_probability -= total_log_prob
+        if rollout.rollout_probability > np.log(1e-4):
             valid_rollouts.append(rollout)
 
-    # Combine the valid rollouts into a final model over prices
-    mean = np.zeros(12)
-    var = np.zeros(12)
-    for rollout in valid_rollouts:
-        print("Prob: ", rollout.rollout_probability)
-        mean += rollout.means * rollout.rollout_probability
-        var += rollout.rollout_probability**2. * rollout.vars
-    for k, val in enumerate(observed):
-        if val is not None:
-            mean[k] = val
-            var[k] = 0.
-    print("Final mean and var: ", mean, var)
-
-    # Finally, draw it
-    plt.figure()
-    plt.grid(True)
-    for k, rollout in enumerate(valid_rollouts):
-        if rollout.rollout_probability > 1E-3:
-            plt.plot(range(12), rollout.means,
-                     color=colors_by_type[rollout.pattern_name],
-                     alpha=0.5, linestyle="--")
-            plt.fill_between(
-                range(12), rollout.means-1.96*np.sqrt(rollout.vars), rollout.means+1.96*np.sqrt(rollout.vars),
-                alpha=0.125, edgecolor=colors_by_type[rollout.pattern_name],
-                facecolor=colormaps_by_type[rollout.pattern_name](float(k)/len(valid_rollouts)))
-
-    plt.plot(range(12), mean, 'k', color=colors_by_type["Average Prediction"])
-    plt.fill_between(range(12), mean-1.96*np.sqrt(var), mean+1.96*np.sqrt(var),
-        alpha=0.25, edgecolor=colors_by_type["Average Prediction"], facecolor=[0.25, 0.1, 0.25])
-
-    label_lines = [Line2D([0], [0], color=colors_by_type[pattern_name], lw=4) for pattern_name in list(colors_by_type.keys())]
-    plt.legend(label_lines, list(colors_by_type.keys()))
+    if model_type == "gmm":
+        plot_gmm_results(valid_rollouts, observed)
+    elif model_type == "kde":
+        plot_kde_results(valid_rollouts, observed)
 
     #plt.figure()
    # bins = np.arange(1., 660, 10.)
